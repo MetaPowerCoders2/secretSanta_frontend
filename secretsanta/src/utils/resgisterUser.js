ee4e497f9931edd8617cccbc3b917f55f6249fc1
Lorena Gil <67319308+logiflo@users.noreply.github.com>
2023-05-31 13:35:05 +0100

    Update cookie domain

--- secretsanta/src/utils/resgisterUser.js
+++ secretsanta/src/utils/resgisterUser.js
@@ -14,8 +14,8 @@ async function registerUser(userObj) {
 async function signIn(userObj) {
   try {
     let userInfo = await fetchData("login/signin", "POST", userObj);
-    cookies.set("user", userInfo);
-    cookies.set("token", userInfo.token);
+    cookies.set("user", userInfo, { domain: ".railway.app" });
+    cookies.set("token", userInfo.token , { domain: ".railway.app" });
     return userInfo;
   } catch (e) {
     console.log(e);
